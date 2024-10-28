ec801da53cd2f7d1a73aa5684d18c0cba78ad3f2
VictorHu93 <victor45663@yahoo.com>
2024-10-28 20:49:05 -0300

    delete

--- core/settings_ci.py
+++
@@ -1,9 +0,0 @@
-from .settings import *  # Importa todas as configurações padrão
-
-# Define o banco de dados como SQLite em memória para testes
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-}
